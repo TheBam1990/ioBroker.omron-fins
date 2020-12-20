b8839628b99a9095d5270925130867c973a39b40
thebam1990 <ulf2356@web.de>
2020-12-20 16:30:01 +0100

    word.words

--- admin/words.js
+++ admin/words.js
@@ -15,37 +15,37 @@ systemDictionary = {
 		"zh-cn": "omron-fins的适配器设置"
 	},
 	"PLC IP Address:": {
-		"en": "PLC IP Address:", 
-		"de": "SPS IP Adresse:",     
-		"ru": "PLC IP адрес:",       
-		"pt": "Endereço IP do PLC:", 
-		"nl": "PLC IP-adres:",       
+		"en": "PLC IP Address:",
+		"de": "SPS IP Adresse:",
+		"ru": "PLC IP адрес:",
+		"pt": "Endereço IP do PLC:",
+		"nl": "PLC IP-adres:",
 		"fr": "Adresse IP de l'API:",
-		"it": "Indirizzo IP del PLC:",  
-		"es": "PLC IP Address:",        
+		"it": "Indirizzo IP del PLC:",
+		"es": "PLC IP Address:",
 		"pl": "Adres IP sterownika PLC:"
 	},
 	"Anschlussnummer:": {
-		"en": "Port number:", 
-		"de": "Anschlussnummer:",     
-		"ru": "Homep nopta:",       
-		"pt": "Endereço IP do PLC:", 
-		"nl": "PLC IP-adres:",       
+		"en": "Port number:",
+		"de": "Anschlussnummer:",
+		"ru": "Homep nopta:",
+		"pt": "Endereço IP do PLC:",
+		"nl": "PLC IP-adres:",
 		"fr": "Numéro de port:",
-		"it": "Numero di porta:",  
-		"es": "Número de puerto:",        
+		"it": "Numero di porta:",
+		"es": "Número de puerto:",
 		"pl": "Numer portu:"
 	},
 	"Poll Intervall(ms):": {
-		"en": "Poll Intervall(ms):", 
-		"de": "Poll Intervall(ms):",     
-		"ru": "????",       
-		"pt": "????", 
-		"nl": "????",       
-		"fr": "???",
-		"it": "??:",  
-		"es": "???:",        
-		"pl": "???:"
+		"en": "Poll Intervall(ms):",
+		"de": "Poll Intervall(ms):",
+		"ru": "Интервал опроса (мс):",
+		"pt": "Intervalo de pesquisa (ms):",
+		"nl": "Poll-interval (ms):",
+		"fr": "Intervalle d'interrogation (ms):",
+		"it": "Intervallo di polling (ms):",
+		"es": "Intervalo de voĉdonado (ms):",
+		"pl": "Okres odpytywania (ms):"
 	}
 	
 };
